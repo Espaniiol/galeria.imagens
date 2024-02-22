7ce37953ea0cba3ad3aa361a023b76ea5a84974f
Espaniiol <guilhermeespaniol@gmail.com>
2024-02-22 19:16:34 -0300

    Segunda Aula

--- scripts.js
+++ scripts.js
@@ -1,7 +1,26 @@
 $(document).ready(function(){
 
-  $('.botao-categoria').on('click', function(){
-    alert($(this).data('categoria'));
-  });
+  const baseImagens = {
+      todas:['imagem.jpg', 'cidade.jpg', 'animal.jpg', 'natureza.jpg'],
+      animais:['animal.jpg', 'animal1.jpg', 'animal2.jpg', 'animal3.jpg'],
+      natureza:['natureza.jpg', 'natureza1.jpg', 'natureza2.jpg', 'natureza3.jpg'],
+      cidade:['cidade.jpg', 'cidade1.jpg', 'cidade2.jpg', 'cidade3.jpg'],
+  }
 
+  function carregarImagens(categoria){
+      const imagens = baseImagens[categoria];
+      const boxImagens = $('body').find('.box-imagens');
+      imagens.forEach(img => {
+          console.log(img);
+          boxImagens.append('<div><img src="imagens/'+img+'" /></div>');
+      });
+  }
+
+  $('.botao-categoria').click(function(){
+      $('body').find('.botao-categoria').removeClass('active');
+      $(this).addClass('active');
+      
+      const categoria = $(this).data('categoria');
+      carregarImagens(categoria);
+    });
 });
